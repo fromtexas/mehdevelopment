e927e3139d332026b31a24612f4529283820f07c
fromtexas <telures@yandex.ru>
2018-03-26 19:01:08 +0300

    text

--- app/sectionTitleAnimation.js
+++ app/sectionTitleAnimation.js
@@ -2,11 +2,6 @@ import {lettersRemove, lettersReveals} from './lettersAnimation';
 
 const notDoneSet = new Set();
 
-//refactoring remove on events, set all in one file
-//folders for js
-//slides counter
-//responsivness
-
 export const titleAnime = (index) => {
     if(index > 0 && !notDoneSet.has(index)){
         let removed = lettersRemove(`.large-word-${index}`);
